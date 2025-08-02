ed64382691a0b8cf276660d2b20dddf2caa2af1f
defrome <iskoblov555@gmail.com>
2025-08-02 23:41:07 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn check_variable(x: i32) {
+    if x == 0 {
+        println!("Переменная равна 0");
+    } else {
+        println!("1");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    
+    // Примеры использования функции
+    check_variable(0);  // Выведет: "Переменная равна 0"
+    check_variable(5);  // Выведет: "1"
+    check_variable(-3); // Выведет: "1"
 }
